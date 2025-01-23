913a8b14164101cbeea0cf8e266208afc7671c09
Lycle <yuliu98@126.com>
2025-01-23 23:44:34 +0200

    Fix typos

--- eval_e_dat_ip.sh
+++ eval_e_dat_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=val_e_i
+#SBATCH --job-name=val_e_ip
 #SBATCH --time=12:00:00
-#SBATCH --mem=128G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=16
 #SBATCH --output=eval_e_ip_%a.out
 #SBATCH --constraint=skl
