87b355de71824161b94a10713267511a276514e5
Lycle <yuliu98@126.com>
2025-01-27 19:19:04 +0200

    Upload eval_ef scripts for sslp

---
+++ eval_ef_dat_sslp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=ef_sslp
+#SBATCH --time=3:30:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=1
+#SBATCH --output=eval_ef_sslp_%a.out
+#SBATCH --constraint=skl
+#SBATCH --partition=batch-skl
+#SBATCH --array=1-120
+
+module load scicomp-python-env
+
+command=$(sed -n "${SLURM_ARRAY_TASK_ID}p" table_ef_sslp.dat | cut -d' ' -f2-)
+srun $command
