03b5d6aee247a4cfc02201448757bacd2a9f08fb
Lycle <yuliu98@126.com>
2025-01-29 14:23:47 +0200

    Upload scripts

---
+++ eval_ef_cflp_25_25.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=ef_cflp_25_25
+#SBATCH --time=4:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=1
+#SBATCH --output=eval_ef_cflp_25_25_%a.out
+#SBATCH --error=eval_ef_cflp_25_25_%a.err
+#SBATCH --constraint=skl
+#SBATCH --partition=batch-skl
+#SBATCH --array=1-9
+
+# Load the required module
+module load scicomp-python-env
+
+# Define the commands to run in an array
+COMMANDS=(
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 100 --test_set 1 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 100 --test_set 2 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 100 --test_set 5 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 100 --test_set 6 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 100 --test_set 7 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 1000 --test_set 1 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 1000 --test_set 3 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 1000 --test_set 7 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_25_25 --n_scenarios 1000 --test_set 8 --n_procs 1"
+)
+
+# Get the command for this job array index
+COMMAND=${COMMANDS[$SLURM_ARRAY_TASK_ID - 1]}
+
+# Execute the command
+echo "Running command: $COMMAND"
+eval $COMMAND
---
+++ eval_ef_cflp_50_50.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=ef_cflp_50_50
+#SBATCH --time=4:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=1
+#SBATCH --output=eval_ef_cflp_50_50_%a.out
+#SBATCH --error=eval_ef_cflp_50_50_%a.err
+#SBATCH --constraint=skl
+#SBATCH --partition=batch-skl
+#SBATCH --array=1-11
+
+# Load the required module
+module load scicomp-python-env
+
+# Define the commands to run in an array
+COMMANDS=(
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 500 --test_set 2 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 500 --test_set 3 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 500 --test_set 5 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 500 --test_set 7 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 500 --test_set 8 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 500 --test_set 9 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 1000 --test_set 2 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 1000 --test_set 4 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 1000 --test_set 5 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 1000 --test_set 7 --n_procs 1"
+    "python3 -m nsp.scripts.evaluate_extensive --problem cflp_50_50 --n_scenarios 1000 --test_set 9 --n_procs 1"
+)
+
+# Get the command for this job array index
+COMMAND=${COMMANDS[$SLURM_ARRAY_TASK_ID - 1]}
+
+# Execute the command
+echo "Running command: $COMMAND"
+eval $COMMAND
