e491f7a16d8cf73a0b854dca6d5f1a11ce3f394f
Lycle <yuliu98@126.com>
2025-02-05 15:53:53 +0200

    Update sh file for ip_i_E train_nn_e

--- train_nn_e_dat_ip.sh
+++ train_nn_e_dat_ip.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=train_e_ip
-#SBATCH --output=train_e_ip_%a.out
+#SBATCH --output=train_e_ip_new.out
 #SBATCH --time=01:00:00
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu-h100-80g
-#SBATCH --array=3
 
 module load scicomp-python-env
 module load cuda/12.2.1
 
-command=$(sed -n "${SLURM_ARRAY_TASK_ID}p" table_train_e_ip.dat | cut -d' ' -f2-)
-srun $command
+srun python3 runner.py --problems ip_i_E --train_nn_e 1
 
