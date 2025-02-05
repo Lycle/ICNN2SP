ba2cd1095365eb0793ca5faea634fc12bbb67d87
Lycle <yuliu98@126.com>
2025-02-05 15:54:49 +0200

    Update sh file for ip_i_E train_nn_e

--- train_nn_e_dat_ip.sh
+++ train_nn_e_dat_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_e_ip
 #SBATCH --output=train_e_ip_new.out
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=1
